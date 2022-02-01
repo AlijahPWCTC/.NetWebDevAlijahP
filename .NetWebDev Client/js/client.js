06d217270115e8eb441feb50d809827dfffead64
Alijah <aperez27@my.wctc.edu>
2022-02-01 12:11:35 -0700

    Finished Assign 2

--- .NetWebDev Client/js/client.js
+++ .NetWebDev Client/js/client.js
@@ -3,19 +3,34 @@ $(function(){
     var toast = new Audio('media/toast.wav');
 
     $('.code').on('click', function(e) {
-        e.preventDefault();
+        
+      e.preventDefault();
           // first pause the audio (in case it is still playing)
           toast.pause();
           // reset the audio
           toast.currentTime = 0;
         // play audio
         toast.play();
-        $('#toast').toast({ autohide: false }).toast('show');
+        
+
+        if($(this).attr('data-button')== 'tistheseasoning'){
+            $('#toastSeasoning').toast({ autohide: false }).toast('show');
+        }
+
+        else if($(this).attr('data-button')== 'gogreen'){
+          $('#toastGreen').toast({ autohide: false }).toast('show');
+        }
+
+        else if($(this).attr('data-button')== 'halfoff'){
+          $('#toastHalf').toast({ autohide: false }).toast('show');
+        }
+        
+      
     });
 
     document.addEventListener('keydown', function(event) {
       if(event.keyCode == 27) {
-        $('#toast').toast('hide');
+        $('.toast').toast('hide');
       }
   });
 });
